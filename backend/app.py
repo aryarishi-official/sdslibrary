6d120ade860c1580d4e8e7ce3c78282c60e1b5b7
Arya <Arya@Rishis-MacBook-Pro.local>
2026-05-26 16:34:47 +0530

    update CORS for vercel frontend

--- backend/app.py
+++ backend/app.py
@@ -32,8 +32,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5174",
-        "https://sdslibrary.vercel.app",],
+    allow_origins=["http://localhost:5174","https://sdslibrary.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
